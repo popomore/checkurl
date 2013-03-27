68f22674a34de9a1692e006288d0358827e7a967
popomore <sakura9515@gmail.com>
2013-03-27 22:05:25 +0800

    add testcase checkSingle

---
+++ test/checkSingle.js
@@ -0,0 +1,28 @@
+var expect = require('expect.js');
+var checkurl = require('../lib/checkurl');
+
+describe('Check single url', function() {
+  it('should check https', function(done) {
+    var url = 'https://a.alipayobjects.com/seajs/1.2.1/sea.js';
+    checkurl(url, function(err, result) {
+      expect(result).to.eql([url, 200]);
+      done();
+    });
+  });
+
+  it('should check http', function(done) {
+    var url = 'http://static.alipayobjects.com/seajs/1.2.1/sea.js';
+    checkurl(url, function(err, result) {
+      expect(result).to.eql([url, 200]);
+      done();
+    });
+  });
+
+  it('should not exist', function(done) {
+    var url = 'http://static.alipayobjects.com/sea.js';
+    checkurl(url, function(err, result) {
+      expect(result).to.eql([url, 404]);
+      done();
+    });
+  });
+});
