37e23fbdfaa3d0838a629b40b3dd7318539b1893
popomore <sakura9515@gmail.com>
2013-03-27 22:11:43 +0800

    add testcase checkCombo

---
+++ test/checkCombo.js
@@ -0,0 +1,16 @@
+var expect = require('expect.js');
+var checkurl = require('../lib/checkurl');
+
+describe('checkCombo', function() {
+  it('should combo', function(done) {
+    var url = 'https://a.alipayobjects.com/??seajs/1.3.1/sea.js,seajs/1.3.1/combo.js,jquery/jquery/1.7.2/jquery.js';
+    checkurl(url, function(err, result) {
+      expect(result).to.eql([
+        ['https://a.alipayobjects.com/seajs/1.3.1/sea.js', 200],
+        ['https://a.alipayobjects.com/seajs/1.3.1/combo.js', 404],
+        ['https://a.alipayobjects.com/jquery/jquery/1.7.2/jquery.js', 200]
+      ]);
+      done();
+    });
+  });
+});
